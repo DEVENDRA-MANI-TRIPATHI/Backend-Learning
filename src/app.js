7b7cbb21c3c56f184cdd0f5ada2c489005a25e32
DEVENDRA-MANI-TRIPATHI <92941880+DEVENDRA-MANI-TRIPATHI@users.noreply.github.com>
2025-03-08 23:24:37 +0530

    standardize API responses,Errors and asyncHandler

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 // configurations of middlewares
 app.use(cors());
 app.use(express.json({ limit: "16kb" })); // for parsing the json data and limit is 16kb if you need more than that adjust it accordingly
-app.use(express.urlencoded({ extendd: true }));
+app.use(express.urlencoded());
 app.use(express.static("public"));  // for saving files on server on temporary basis
-app.use(cookieParser({ limit: "16kb" }));  //for ending the cookies from backend to frontend and vice versa
+app.use(cookieParser());  //for ending the cookies from backend to frontend and vice versa
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,16 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+
+
+// const asyncHandler = (fun) => { }
+// const asyncHandler = (fun) => { () => { } } if we are passing function inside the function
+// const asyncHandler = () => ()=>{}   we car write the above function like this one as well
+
+
+
+
+export {asyncHandler}
