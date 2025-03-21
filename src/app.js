539a9a7c3cc17e2eada46277996c187d55158e1b
DEVENDRA-MANI-TRIPATHI <92941880+DEVENDRA-MANI-TRIPATHI@users.noreply.github.com>
2025-03-21 18:04:53 +0530

    routes configuration

--- src/app.js
+++ src/app.js
@@ -11,6 +11,12 @@ app.use(express.urlencoded());
 app.use(express.static("public"));  // for saving files on server on temporary basis
 app.use(cookieParser());  //for ending the cookies from backend to frontend and vice versa
 
+// routes import
+import userRouter from "./routes/user.routes.js"
 
+
+
+//routes declaration
+app.use("/api/v1/user",userRouter)
 
 export default app;
