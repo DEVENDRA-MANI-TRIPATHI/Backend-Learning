554612332716fff9707d074b15e98a09a1e8010c
DEVENDRA-MANI-TRIPATHI <92941880+DEVENDRA-MANI-TRIPATHI@users.noreply.github.com>
2025-03-25 16:02:12 +0530

    added watchhistory and channel info controller and added all the routes as well

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logOutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
+    loginUser,
+    logOutUser,
+    refreshAccessToken,
+    registerUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
+} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyjwt } from "../middlewares/auth.middleware.js";
 
@@ -22,5 +34,12 @@ router.route("/login").post(loginUser)
 // Secure Routes
 router.route("/logout").post(verifyjwt, logOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyjwt, changeCurrentPassword)
+router.route("/current-user").get(verifyjwt, getCurrentUser)
+router.route("/update-account").patch(verifyjwt, updateAccountDetails)
+router.route("/avatar").patch(verifyjwt,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyjwt, upload.single("coverImage"), updateUserCoverImage)
+router.route("/channel/:username").get(verifyjwt, getUserChannelProfile)
+router.route("/watchHistory").get(verifyjwt,getWatchHistory)
 
 export default router
